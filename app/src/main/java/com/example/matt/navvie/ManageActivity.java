3e0e9042b2ca347a91cda783ff47d429527eadc7
Mattm11888 <mattm11888@gmail.com>
2016-03-11 00:34:47 -0500

    small edits to manage activity

--- app/src/main/java/com/example/matt/navvie/ManageActivity.java
+++ app/src/main/java/com/example/matt/navvie/ManageActivity.java
@@ -1,18 +1,27 @@
 package com.example.matt.navvie;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ManageActivity extends AppCompatActivity {
     private String[] friends,requests,curFriends;
@@ -28,16 +37,30 @@ public class ManageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_manage);
 
         cancelManageButton = (Button) findViewById(R.id.cancelManageButton);
+
         cancelManageButton.setOnClickListener(new buttonListener());
+
+
 
         friendListView = (ListView) findViewById(R.id.listView2);
         searchText = (EditText) findViewById(R.id.searchFriendText);
 
         requestsListView = (ListView) findViewById(R.id.listView1);
         initRequestList(); /// creates request list
+        requestsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // Toast.makeText(ManageActivity.this, "List item was clicked at " + position, Toast.LENGTH_SHORT).show();
+                requestsList.remove(position);
+                requestsAdapter.notifyDataSetChanged();
+            }
+        });
 
         curFriendsListView = (ListView) findViewById(R.id.listView3);
         curSearchText = (EditText) findViewById(R.id.manageFriendText);
+
+
+
 
         curSearchText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -130,7 +153,8 @@ public class ManageActivity extends AppCompatActivity {
         requests = new String[]{"Daniel Hill","Cory Sabel", "Andrew Schaefer"};
         requestsList = new ArrayList<>(Arrays.asList(requests));
         requestsAdapter = new ArrayAdapter<String>(this, R.layout.list_requests, R.id.requestsText, requestsList);
-        requestsListView.setAdapter(requestsAdapter);
+        //requestsListView.setAdapter(requestsAdapter);
+        requestsListView.setAdapter(new MyListAdapter(this,R.layout.list_requests, requestsList));
     }
 
     public void initManageList(){
@@ -149,7 +173,63 @@ public class ManageActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                     break;
+                case R.id.acceptButton:
+                    //friendList.remove(2);
+                    break;
             }
         }
     }
-}
+
+    private class MyListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        private List<String> mObjects;
+        public MyListAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+
+                viewHolder.title = (TextView) convertView.findViewById(R.id.requestsText);
+                viewHolder.accButton = (Button) convertView.findViewById(R.id.acceptButton);
+                viewHolder.decButton = (Button) convertView.findViewById(R.id.declineButton);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.accButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Accept Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                    requestsList.remove(position);
+                    requestsAdapter.notifyDataSetChanged();
+                }
+            });
+            mainViewholder.decButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Decline Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                    requestsList.remove(position);
+                    requestsAdapter.notifyDataSetChanged();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+
+    }
+    public class ViewHolder {
+        TextView title;
+        Button accButton;
+        Button decButton;
+    }
+    }
+
+
+
