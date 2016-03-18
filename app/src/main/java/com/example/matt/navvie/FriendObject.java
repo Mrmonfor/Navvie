4325e20ca0e5207e70009450236a7571f90363e3
Mattm11888 <mattm11888@gmail.com>
2016-03-18 13:15:05 -0400

    Server testing

---
+++ app/src/main/java/com/example/matt/navvie/FriendObject.java
@@ -0,0 +1,11 @@
+package com.example.matt.navvie;
+
+/**
+ * Created by Matt on 3/18/2016.
+ */
+public class FriendObject {
+
+    public FriendObject(String FName, String LName, double longCord, double latCord,String locName, String status){
+
+    }
+}
