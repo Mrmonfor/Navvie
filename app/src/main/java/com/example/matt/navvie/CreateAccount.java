e7d636ee46be51c6583340abb9663c3d28359705
chase <nottapesahc@gmail.com>
2016-03-26 22:39:01 -0400

    CreateAccount networking tweaks

--- app/src/main/java/com/example/matt/navvie/CreateAccount.java
+++ app/src/main/java/com/example/matt/navvie/CreateAccount.java
@@ -3,6 +3,7 @@ package com.example.matt.navvie;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,10 +15,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.URL;
 import java.net.UnknownHostException;
 
@@ -26,6 +29,10 @@ public class CreateAccount extends Activity {
     private Button SubmitButton, CancelButton2;
     private EditText fName, lName, pword, pword2, email;
     private boolean send = false;
+    private boolean threadIsFinished= false;
+    private String requestResponse= null;
+    private boolean accountCreated = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,42 +45,43 @@ public class CreateAccount extends Activity {
         pword = (EditText) findViewById(R.id.CreatePass);
         pword2 = (EditText) findViewById(R.id.RetypePass);
         email = (EditText) findViewById(R.id.EnterEmail);
-
         SubmitButton.setOnClickListener(new buttonListener());
         CancelButton2.setOnClickListener(new buttonListener());
 
         final Thread udpThread = new Thread(new Runnable() {
             @Override
             public void run(){
-
+                Looper.prepare();
+                DatagramSocket socket;
                 while(true){
                     //maybe sleep(100)
                     if(send){
 
                         try {
-
                             InetAddress server = InetAddress.getByName("162.243.203.154"); //server ip
                             int servPort = 3020; //server port
                             Log.d("UDP","Connection...");
-                            DatagramSocket socket = new DatagramSocket(); //client socket
-
-                            //createuser -> test
-                            String output = ",test,"+fName.getText().toString() + "," +
+                            socket = new DatagramSocket(); //client socket
+                            //createuser -> ,test,
+                            int localPort = socket.getLocalPort();
+                            String output = "test,"+fName.getText().toString() + "," +
                                     lName.getText().toString() + "," +
                                     pword.getText().toString() + "," +
                                     email.getText().toString()+",";
                             //
                             byte[] buffer = output.getBytes();
                             DatagramPacket packet = new DatagramPacket(buffer,buffer.length, server, servPort);
-
                             socket.send(packet);
 
                             packet.setData(new byte[50]); //this needs to be set to some other value probably
+                            String incomingData2 = "";
+                            String incomingData = "";
+                            //response 1
                             while(true){
-                                Thread.sleep(1);
+                                //Thread.sleep(1000);
                                 try {
                                     socket.receive(packet);
-                                    String incomingData = new String(packet.getData());
+                                    incomingData = new String(packet.getData());
                                     if(incomingData.compareTo(output)!=0){
                                         Log.d("UDP", incomingData); //might not be right
                                         break;
@@ -88,15 +96,36 @@ public class CreateAccount extends Activity {
                                 //by a certain amount of time
                             }
                             socket.close();
+                            //response 2
+                            socket = new DatagramSocket(localPort);
+                            String port = incomingData.substring(0, 4);
+                            packet.setPort(Integer.parseInt(port));
+                            socket.send(packet);
+                            while(true){
+                                try{
+                                    //wait(2000);
+                                    socket.receive(packet);
+                                    incomingData2 = new String(packet.getData());
+                                    if(incomingData2.compareTo(incomingData)!=0){
+                                        Log.d("UDP loop 2", incomingData2.substring(0,7));
+                                        //do something with incomingData2
+                                        if(incomingData2.substring(0,7).equals("success")){
+                                            accountCreated = true;
+                                        }
+                                        threadIsFinished = true;
+                                        break;
+                                    }
+                                } catch(IOException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                            socket.close();
                             send = false; //break out of loop
                             Log.d("UDP", "COMPLETED!");
-                            Intent intent = new Intent(CreateAccount.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
+
                         } catch(Exception e) {
                             e.printStackTrace();
                         }
-
                     }
                 }
             }
@@ -112,8 +141,22 @@ public class CreateAccount extends Activity {
                 case R.id.SubmitButton:
                     if(pword.getText().toString().equals(pword2.getText().toString())) {
                         send = true;
-                        Toast toast = Toast.makeText(getApplicationContext(), "Wait for a response from the server", Toast.LENGTH_SHORT);
-                        toast.show();
+                        //Toast toast = Toast.makeText(getApplicationContext(), "Wait for a response from the server", Toast.LENGTH_SHORT);
+                        //toast.show();
+                        while(!threadIsFinished){
+
+                        }
+                        if(accountCreated) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Account created.", Toast.LENGTH_SHORT); //check email?
+                            toast.show();
+                        }
+                        else{
+                            Toast toast = Toast.makeText(getApplicationContext(), "Account not created. Check your details.", Toast.LENGTH_SHORT); //check email?
+                            toast.show();
+                        }
+                        Intent intent = new Intent(CreateAccount.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
                     }
                     else{
                         Toast toast = Toast.makeText(getApplicationContext(), "The passwords don't match.", Toast.LENGTH_LONG);
